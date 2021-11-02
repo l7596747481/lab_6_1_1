6f16c9f19640077dd7c0a91d15023398d7aba680
bwaah1 <chykivan@gmail.com>
2021-11-02 22:57:11 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6_1_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			const int n = 6;
+			int a[n] = { 9, 8, 7, 6, 5, 4 };
+			t = Sum(a, n);
+			Assert::AreEqual(21, t);
+		}
+	};
+}
---
+++ lab_6_1_1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int* a, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		a[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << a[i];
+	cout << endl;
+}
+int Number(int* a, const int size)
+{
+	int count = 0;
+	for (int i = 0; i < size; i++)
+		if (a[i] % 2 == 1 && a[i] > 0)
+			count++;
+	return count;
+}
+int Sum(int* a, const int size)
+{
+	int S = 0;
+	for (int i = 0; i < size; i++)
+		if (a[i] % 2 == 1 && a[i] > 0)
+			S += a[i];
+	return S;
+}
+void Mode(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+		if (a[i] % 2 == 1 && a[i] > 0)
+			a[i] = 0;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 23;
+	int a[n];
+	int Low = -25;
+	int High = 80;
+	Create(a, n, Low, High);
+	Print(a, n);
+	cout << "  " << "N = " << Number(a, n) << endl;
+	cout << "  " << "S = " << Sum(a, n) << endl;
+	Mode(a, n);
+	Print(a, n);
+	return 0;
+}
